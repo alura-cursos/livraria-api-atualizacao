b0edf93caa4046756f0e64848a8ac8d414a1ebbe
Camila <camilabentespessoa@gmail.com.br>
2022-08-19 04:34:23 -0300

    aula02

--- src/produtos.controller.ts
+++ src/produtos.controller.ts
@@ -1,34 +1,39 @@
 /* eslint-disable prettier/prettier */
 import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
+import { Produto } from './produto.model';
 
 @Controller('produtos')
 export class ProdutosController {
+
+  produtos: Produto[] = [
+    new Produto("LIV01", "Livro TDD e BDD na prática", 29.90),
+    new Produto("LIV02", "Livro Iniciando com Flutter", 39.90),
+    new Produto("LIV03", "Inteligência artificial como serviço", 29.90)
+  ];
+
   @Get()
-  obterTodos(): string {
-    return 'Lista todos os produtos';
+  obterTodos(): Produto[] {
+    return this.produtos;
   }
 
   @Get(':id')
-  obterUm(@Param() params): string {
-    console.log(params.id);
-    return `Retorna os dados do produto ${params.id}`;
+  obterUm(@Param() params): Produto {
+      return this.produtos[0];
   }
 
   @Post()
-  criar(@Body() produto): string {
-    console.log(produto);
-    return 'Produto criado';
+  criar(@Body() produto: Produto) {
+    produto.id = 100;
+    this.produtos.push(produto);
   }
 
   @Put()
-  alterar(@Body() produto): string {
-    console.log(produto);
-    return 'Produto alterado';
+  alterar(@Body() produto: Produto): Produto {
+    return produto;
   }
 
   @Delete(':id')
-  apagar(@Param() params): string {
-    console.log(params.id);
-    return `Apaga o produto ${params.id}`;
+  apagar(@Param() params) {
+    this.produtos.pop();
   }
 }
