896e7661ee5edd34c428dd2388a4781508fee865
Camila <camilabentespessoa@gmail.com.br>
2022-08-22 17:45:33 -0300

    aula05-final

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,16 +6,18 @@ import { AppService } from './app.service';
 import { LivrosController } from './livros.controller';
 import { LivrosService } from './livros.service';
 import { SequelizeModule } from '@nestjs/sequelize/dist/sequelize.module';
+import { ConfigModule } from '@nestjs/config'
 import { Livro } from './livro.model';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     SequelizeModule.forRoot({
       dialect: 'mysql',
       host: 'localhost',
       port: 3307,
-      username: 'root',
-      password: 'admin',
+      username: process.env.USUARIO_BANCO_DE_DADOS,
+      password: process.env.SENHA_BANCO_DE_DADOS,
       database: 'livraria',
       autoLoadModels: true,
       synchronize: true,
